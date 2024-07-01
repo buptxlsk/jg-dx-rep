8672d7acf7749c622d60c367dd6585be4226f43a
bangx <3274739376@qq.com>
2024-07-01 09:22:27 +0800

    训练模型

--- train.py
+++ train.py
@@ -1,4 +1,14 @@
+import torch
 from ultralytics import YOLO
+
+# 检查CUDA是否可用
+if torch.cuda.is_available():
+    device = 'cuda'
+else:
+    device = 'cpu'
+
+# 打印所使用的设备
+print(f'Using device: {device}')
 
 # Load a model
 model = YOLO("yolov8n.yaml")  # build a new model from YAML
@@ -6,4 +16,4 @@ model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 model = YOLO("yolov8n.yaml").load("yolov8n.pt")  # build from YAML and transfer weights
 
 # Train the model
-results = model.train(data="jg.yaml", epochs=100)
+results = model.train(data="jg.yaml", epochs=100, device=device)
